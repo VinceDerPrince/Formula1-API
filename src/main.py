2d865368b2614d694d0029511bfa2044505a9c92
VinceDerPrince <vincent.elster@gmail.com>
2022-07-06 22:20:43 +0200

    qualifying results

--- src/main.py
+++ src/main.py
@@ -8,5 +8,9 @@ async def route():
     return {"message":"Welcome to this unoffical Formula1 API"}
 
 @app.get("/get_race_results")
-async def get_race_results(year: int, grand_prix: str):
-    return _scraper.get_all_results(year, grand_prix)
+async def get_race_results(year: int, track: str):
+    return _scraper.get_race_results(year, track)
+
+@app.get("/get_quali_results")
+async def get_quali_results(year: int, track: str):
+    return _scraper.get_qualifying_results(year, track)
--- src/scraper.py
+++ src/scraper.py
@@ -1,10 +1,11 @@
+import chunk
 from typing import Dict,List
 import requests as _requests
 import bs4 as _bs4
 
-def _generate_url(year: int, place:str) -> str:
+def _generate_url(year: int, place: str, race_type: str) -> str:
     place = place.replace(" ", "-")
-    url = f"https://www.bbc.com/sport/formula1/{year}/{place}-grand-prix/results"
+    url = f"https://www.bbc.com/sport/formula1/{year}/{place}-grand-prix/results/{race_type}"
     return url
 
 def _get_page(url: str) -> _bs4.BeautifulSoup:
@@ -12,9 +13,9 @@ def _get_page(url: str) -> _bs4.BeautifulSoup:
     soup = _bs4.BeautifulSoup(page.content, "html.parser")
     return soup
 
-def get_all_results(year: int, place: str) -> Dict:
+def get_race_results(year: int, place: str) -> Dict:
     #return results of a specific race
-    url = _generate_url(year, place)
+    url = _generate_url(year, place, "race")
     page = _get_page(url)
     raw_placements = page.find_all(class_ = "gs-u-vh")
     placements = [placement.text for placement in raw_placements]
@@ -23,15 +24,33 @@ def get_all_results(year: int, place: str) -> Dict:
     final = dict()
     for i in range(len(chunks)):
         final[i+1] = {
-            "driver": chunks[i][0],
-            "number": chunks[i][1],
-            "grid": chunks[i][2],
-            "pits": chunks[i][3],
-            "fastest-lap": chunks[i][4],
-            "race-time": chunks[i][5],
-            "points": chunks[i][6]
+            "driver": chunks[i][1],
+            "number": chunks[i][2],
+            "grid": chunks[i][3],
+            "pits": chunks[i][4],
+            "fastest-lap": chunks[i][5],
+            "race-time": chunks[i][6],
+            "points": chunks[i][7]
         }
     
     return final
 
-print(get_all_results(2022,"british"))
+def get_qualifying_results(year: int, place: str) -> Dict:
+    #return results of a specific race
+    url = _generate_url(year, place, "qualifying")
+    page = _get_page(url)
+    raw_placements = page.find_all(class_ = "gs-u-vh")
+    placements = [placement.text for placement in raw_placements]
+    placements = placements[6:]
+    chunks = [placements[x:x+7] for x in range(0, len(placements), 7)]
+    final = dict()
+    for i in range(len(chunks)):
+        final[i+1] = {
+            "driver": chunks[i][1],
+            "number": chunks[i][2],
+            "q1": chunks[i][3],
+            "q2": chunks[i][4],
+            "q3": chunks[i][5],
+        }
+    
+    return final
